4dfd489f5754507e364d25c0e35a240d8c33d851
Patrick_Laptop <patrick.rainer2@gmail.com>
2021-01-09 09:11:37 +0100

    Started

---
+++ EasyBackup/Services/BackupTimeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using EasyBackup.Models;
+
+namespace EasyBackup.Services
+{
+    public class BackupTimeChecker
+    {
+        public List<BackupCase> BackupCases { get; }
+
+        public BackupTimeChecker(List<BackupCase> backupCases)
+        {
+            BackupCases = backupCases;
+
+            var timer = new Timer(state => CheckIsBackupDue());
+            timer.Change(0, 60000);
+        }
+
+        void CheckIsBackupDue()
+        {
+            foreach (var backupCase in BackupCases)
+            {
+                //if IsBackupCase due
+                //Add to Backup to backupQueue
+            }
+        }
+    }
+}
