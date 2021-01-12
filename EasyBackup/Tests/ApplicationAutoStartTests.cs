89bc6e2eeaaa4fc3ed64d363f3d99583560ebe4e
Patrick Rainer <patrick.rainer@sulzer.com>
2021-01-12 13:07:25 +0100

    Wrote Tests

---
+++ EasyBackup/Tests/ApplicationAutoStartTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyBackup.Services;
+using NUnit.Framework;
+
+namespace EasyBackup.Tests
+{
+    [TestFixture]
+    public class ApplicationAutoStartTests
+    {
+        [Test]
+        public void SaveAutoStartToRegistry()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void LoadAutoStartFromRegistry()
+        {
+            var result = RegistryHelper.IsApplicationRegisteredForStartUp();
+            Assert.Fail();
+        }
+    }
+}
